100a3e18cd561780bea4ff69c14cc655aec12757
ori <orib@aetrex.co.il>
2019-06-05 17:13:24 +0300

    updates for auto complete component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import {HttpClientModule} from '@angular/common/http';
 
@@ -23,6 +23,7 @@ import { AutoCompleteComponent } from './components/auto.complete/auto.complete.
         BrowserModule,
         AppRoutingModule,
         FormsModule,
+        ReactiveFormsModule,
         HttpClientModule
     ],
     providers: [
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {CountriesService} from '../../services/countries/countries.service';
 import {CitiesService} from '../../services/cities/cities.service';
+import {FormControl, FormGroup} from "@angular/forms";
 
 @Component({
     selector: 'app-home',
@@ -15,17 +16,42 @@ export class HomeComponent implements OnInit {
     public _currentCity: any = '';
     public _countriesService: CountriesService;
     public _citiesService: CitiesService;
+    public _countryOptions: any = {};
+    public _citiesOptions: any = {};
+    public test: any = '';
     constructor(countriesService: CountriesService, citiesService: CitiesService) {
         this._countriesService = countriesService;
         this._citiesService = citiesService;
     }
 
     ngOnInit() {
+        this._countryOptions = {
+            list: [],
+            prefix: 'prefix',
+            itemKey: 'name',
+            inputClass: '',
+            placeholder: 'Countries',
+            inputName: 'countries',
+            required: true,
+            onNgClass: {'empty': !this._currentCountry}
+        };
+        this._citiesOptions = {
+            list: [],
+            prefix: 'prefix 2',
+            itemKey: '',
+            inputClass: '',
+            placeholder: 'Cities',
+            inputName: 'cities',
+            required: true,
+            onNgClass: {'empty': !this._currentCity}
+        };
+
         this._countriesService.getCountries().subscribe(
             (data: any) => {
                 console.log(data);
                 if (!data.err) {
                     this._countries = data.countries;
+                    this._countryOptions.list = this._countries;
                 }
             },
             (error: any) => {
@@ -37,6 +63,9 @@ export class HomeComponent implements OnInit {
                 console.log(data);
                 if (!data.err) {
                     this._cities = data.cities;
+                    if (this._currentCountry.key) {
+                        this._citiesOptions.list = this._cities[this._currentCountry.key];
+                    }
                 }
             },
             (error: any) => {
@@ -47,6 +76,15 @@ export class HomeComponent implements OnInit {
 
     onCountryChange() {
         this._currentCity = '';
+        setTimeout(() => {
+            if (this._currentCountry.key) {
+                this._citiesOptions.list = this._cities[this._currentCountry.key];
+            }
+        });
+    }
+
+    submit(form: any) {
+        console.log('form', form)
     }
 
 }
